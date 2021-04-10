0c32888bf8aae9068122a1b9319d5680d9d827ea
Tadahiro Kato <goemon12.tc4@gmail.com>
2021-04-10 21:58:07 +0900

    Initial Commit

---
+++ RentalDoc/RentalDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalDocApp.swift
+//  RentalDoc
+//
+//  Created by goemon12 on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct RentalDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
